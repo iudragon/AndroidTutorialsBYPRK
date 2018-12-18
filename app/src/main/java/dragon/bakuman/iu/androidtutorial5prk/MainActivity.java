78d8ce49d52d25a8b44524832343e8ffa984a641
TeamLewok <42891947+TeamLewok@users.noreply.github.com>
2018-12-18 15:03:16 +0530

    7 Send String message to other Activity. Intent putExtra.

--- app/src/main/java/dragon/bakuman/iu/androidtutorial5prk/MainActivity.java
+++ app/src/main/java/dragon/bakuman/iu/androidtutorial5prk/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 
 // CTRL ALT O to remove unwanted imports!
@@ -19,8 +20,19 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void sendMessage(View view) {
+        //send a string to another activity.
+        //find editText widget by finViewById
+        EditText editText  = findViewById(R.id.user_message);
+
+        //store whats in the editText in the String "message"
+        String message = editText.getText().toString();
 
         Intent intent = new Intent(this, MessageActivity.class);
+
+        //put the "message" in the intent
+        //CTRL + P shortcut to view Parameter for method
+        intent.putExtra("EXTRA_MESSAGE", message);
+
         startActivity(intent);
 
     }
--- app/src/main/java/dragon/bakuman/iu/androidtutorial5prk/MessageActivity.java
+++ app/src/main/java/dragon/bakuman/iu/androidtutorial5prk/MessageActivity.java
@@ -1,7 +1,9 @@
 package dragon.bakuman.iu.androidtutorial5prk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MessageActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+        //get the intent that started this activity
+        Intent intent = getIntent();
+
+        //now fetch the extra message from the intent
+        String message = intent.getStringExtra("EXTRA_MESSAGE");
+
+        //now display this message on TextView
+        TextView textView = findViewById(R.id.display_message);
+        textView.setText(message);
     }
+
+
+
 }
