b9ce79f5a7b90e0fc84425e1a52dfe625fa49d7b
TeamLewok <42891947+TeamLewok@users.noreply.github.com>
2018-12-18 14:43:54 +0530

    6

--- app/src/main/java/dragon/bakuman/iu/androidtutorial5prk/MainActivity.java
+++ app/src/main/java/dragon/bakuman/iu/androidtutorial5prk/MainActivity.java
@@ -1,10 +1,12 @@
 package dragon.bakuman.iu.androidtutorial5prk;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+
+
+// CTRL ALT O to remove unwanted imports!
 
 public class MainActivity extends AppCompatActivity {
 
